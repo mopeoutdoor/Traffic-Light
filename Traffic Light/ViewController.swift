571c69012105b2874f419ff08cdb5dabb0d8a3b9
Mikhail Scherbina <irinakop@air-irina.lan>
2020-01-29 16:33:34 +0300

    Initial Commit

---
+++ Traffic Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Traffic Light
+//
+//  Created by Irina Kopchenova on 29.01.2020.
+//  Copyright © 2020 Mikhail Scherbina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
