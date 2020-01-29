279af38ae43d55cb7b3bb2f3c07131709f8d60a4
Mikhail Scherbina <irinakop@air-irina.lan>
2020-01-30 01:40:48 +0300

    Final

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var redCircleView: UIImageView!
+    @IBOutlet weak var yellowCircleView: UIImageView!
+    @IBOutlet weak var greenCircleView: UIImageView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+            redCircleView.alpha = 0.3
+            yellowCircleView.alpha = 0.3
+            greenCircleView.alpha = 0.3
     }
 
-
+    @IBAction func tappedButton(_ sender: UIButton) {
+        switch (redCircleView.alpha,yellowCircleView.alpha,greenCircleView.alpha) {
+        case (1.0,_,_):
+            redCircleView.alpha = 0.3
+            yellowCircleView.alpha = 1.0
+        case (_,1.0,_):
+            yellowCircleView.alpha = 0.3
+            greenCircleView.alpha = 1.0
+        case (_,_,1.0):
+            greenCircleView.alpha = 0.3
+            redCircleView.alpha = 1.0
+        default:
+            startButton.setTitle("Next", for: .normal)
+            redCircleView.alpha = 1.0
+        }
+        
+    }
+    
+    
 }
 
